f21f1ab952bfdcbed2667b606ee5b3aac15b9bac
Tomas Sargiotto <tomas.sargiotto@mastermetrics.com>
2024-02-06 20:24:14 -0300

    enhance pdf download component

--- src/components/PdfDownload/PdfDownloadV2.tsx
+++ src/components/PdfDownload/PdfDownloadV2.tsx
@@ -1,15 +1,14 @@
 import { registerTypesLabels } from "../../class/Profile/types/RegisterTypes";
 import { CuitAccount } from "../../store/CuitSlice";
 import { convertDateToDDMMAAAASeparated2 } from "../../utils/utils";
-import { lastDayOfMonth } from "date-fns";
-import './index.css';
-import InvoiceV1 from "../../class/Invoice/InvoiceV1";
+import './pdfDownloadV2Styles.css';
 import Invoice from "../../class/Invoice/Invoice";
+import { format, parseISO } from "date-fns";
 
 export const invoiceTypes = {
   C: 11,
-  A:1,
-  B: 6,
+  A: '01',
+  B: '006',
   NOTA_CREDITO_A: 3,
   NOTA_CREDITO_B: 8,
   NOTA_CREDITO_C: 13,
@@ -24,7 +23,24 @@ const invoiceCodes = {
   NOTA_CREDITO_C: 'C',
 }
 
+export const vatValues: {[k:string]: number} = {
+  'No gravado': 0,
+  'Excento': 0,
+  0: 0,
+  10.5: 10.5,
+  21: 21,
+  27: 27,
+  5: 5,
+  2.5: 2.5
+}
+
 const copies = ['ORIGINAL', 'DUPLICADO', 'TRIPLICADO']
+
+const transformDigitNumber = (number: number, digits: number, fill=0) => {
+  const currentLength = number.toString().length
+  const missingNumbers = digits - currentLength
+  return fill.toString().repeat(missingNumbers)+number
+}
 
 function PDFDownloadV2({invoice, cuit}: {invoice: Invoice, cuit: CuitAccount}) {
     const invoiceDate = new Date(invoice.date)
@@ -38,38 +54,46 @@ function PDFDownloadV2({invoice, cuit}: {invoice: Invoice, cuit: CuitAccount}) {
         ? 'FACTURA'
         : 'NOTA CREDITO'
 
-    const total = invoice.items.reduce((amount: {iva: {[k: string]: number}, grossAmount: number, netAmount: number}, item) => {
-      if (!amount.iva[item.iva!]) amount.iva[item.iva!] = 0
-      amount.iva[item.iva!] += item.units! * item.unitValue! * (item.iva! / 100)
-      amount.grossAmount += Math.round((item.units! * item.unitValue! * ((100 - item.iva!) / 100)*100))/100
+    const total = invoice.items.reduce((amount: {iva: {[k: string]: number}, grossAmount: number, netAmount: number, excentAmount: number, notTaxedAmount: number}, item) => {
       amount.netAmount += item.units! * item.unitValue!
+      if (item.iva?.toString().toLowerCase() === 'excento') {
+        amount.excentAmount += item.units! * item.unitValue!
+        return amount
+      }
+      if (item.iva?.toString().toLowerCase() === 'no gravado') {
+        amount.notTaxedAmount += item.units! * item.unitValue!
+        return amount
+      }
+      if (!amount.iva[item.iva!]) amount.iva[item.iva!] = 0
+      amount.iva[item.iva!] += item.units! * item.unitValue! * (vatValues[item.iva!] / 100)
+      amount.grossAmount += Math.round((item.units! * item.unitValue! * ((100 - vatValues[item.iva!]) / 100)*100))/100
       return amount
-  }, {iva: {}, grossAmount: 0, netAmount: 0})
+  }, {iva: {}, grossAmount: 0, netAmount: 0, excentAmount: 0, notTaxedAmount: 0})
     return (
       <>
         {copies.map((copy) => {
           return (
-              <div className='pdf-download'>
+              <div className='pdf-download-v2'>
                 <div className="borderDiv originalLabel">
                   {copy}
                 </div>
               <div className="invoiceHeaderContainer">
                 <div className="invoiceTypeContainer">
                   <div className="invoiceType">{invoiceCodes[invoice.invoiceType]}</div>
-                  <div>COD. {cod}</div>
+                  <div className="invoice-code">COD. {cod}</div>
                 </div>     
                 <div className="invoiceHeaderContent">
                   <div className="fromName">{cuit.fullname}</div>
-                    <div className="inf">
-                      {/* <div className="label">Razón Social: <span className="data">{cuit.fullname}</span></div> */}
-                      <div className="label">Domicilio Comercial: <span className="data">{cuit.address}</span></div>
-                      <div className="label">Condición frente al IVA: {registerTypesLabels[cuit.registerType]}</div>
-                    </div>
+                  <div className="inf">
+                    {/* <div className="label">Razón Social: <span className="data">{cuit.fullname}</span></div> */}
+                    <div className="label">Domicilio Comercial: <span className="data">{cuit.address}</span></div>
+                    <div className="label">Condición frente al IVA: {registerTypesLabels[cuit.registerType]}</div>
+                  </div>
                 </div>
                 <div className="invoiceHeaderContent">
                   <div className="fromName">{invoiceType}</div>
                   <div className="inf">
-                    <div className="label">Punto de Venta: {cuit.salePoint} - Comp. Nro: {invoice.number}</div>
+                    <div className="label">Punto de Venta: {cuit.salePoint} - Comp. Nro: {transformDigitNumber(invoice.number,8)}</div>
                     <div className="label">Fecha Emisión: <span className="data">{convertDateToDDMMAAAASeparated2(invoiceDate)}</span></div>
                     <div className="label">CUIT: <span className="data">{cuit.cuit}</span></div>
                     <div className="label">Ingresos Brutos: <span className="data">{cuit.cuit}</span></div>
@@ -79,10 +103,10 @@ function PDFDownloadV2({invoice, cuit}: {invoice: Invoice, cuit: CuitAccount}) {
               </div>
               <div className="borderDiv periodContainer">
                 <div className="label">Periodo Facturado Desde: 
-                  {perdesDate && <span className="data">{convertDateToDDMMAAAASeparated2(perdesDate)}</span>}
+                  {perdesDate && <span className="data date-period">{convertDateToDDMMAAAASeparated2(perdesDate)}</span>}
                 </div>
                 <div className="label">Hasta: 
-                  {perhasDate && <span className="data">{convertDateToDDMMAAAASeparated2(perhasDate.toISOString())}</span>}
+                  {perhasDate && <span className="data date-period">{convertDateToDDMMAAAASeparated2(perhasDate.toISOString())}</span>}
                 </div>
               </div>
               <div className="borderDiv cuit-data">
@@ -92,28 +116,32 @@ function PDFDownloadV2({invoice, cuit}: {invoice: Invoice, cuit: CuitAccount}) {
                     invoice.destinataryDocumentType === '96' ? 'DNI:' : 'CUIT:'
                   }
                   <span className="data">{invoice.destinataryDocument}</span></div>
-                  <div className="label">Apellido y Nombre / Razon Social: <span className="data">{invoice.destinatary}</span></div>
-                </div>
-                <div className="destinataryInfoContainer">
                   <div className="label">Condición frente al IVA: <span className="data">{['A', 'NOTA_CREDITO_A'].includes(invoice.invoiceType) ? 'Responsable Inscripto' : 'Consumidor Final'}</span></div>
-                  <div className="label">Domicilio: <span className="data"> - </span></div>
+                  <div className="label">Condición de venta: <span className="data">Otras</span></div>
                 </div>
                 <div className="destinataryInfoContainer">
-                  <div className="label">Condición de venta: <span className="data">Otras</span></div>
+                  <div className="label">Apellido y Nombre / Razon Social: <span className="data">{invoice.destinatary}</span></div>
+                  <div className="label">Domicilio: <span className="data"> {invoice.destinataryAddress || '-'} </span></div>
                 </div>
               </div>
               <div className="tableContainer">
                 <table>
                   <thead>
                     <tr>
-                      <th>Cód.</th>
-                      <th>Producto / Servicio</th>
-                      <th>Cant.</th>
-                      <th>U. Medida</th>
+                      <th>Código</th>
+                      <th className="table-description">Producto / Servicio</th>
+                      <th>Cantidad</th>
+                      <th>U. medida</th>
                       <th>Precio Unit.</th>
                       <th>% Bonif</th>
                       <th>Imp. Bonif.</th>
                       <th>Subtotal</th>
+                      {invoice.invoiceType === 'A' && (
+                        <>
+                          <th>Alicuota IVA</th>
+                          <th>Subtotal c/IVA</th>
+                        </>
+                      )}
                     </tr>
                   </thead>
                   <tbody>
@@ -121,13 +149,19 @@ function PDFDownloadV2({invoice, cuit}: {invoice: Invoice, cuit: CuitAccount}) {
                       return (
                         <tr>
                           <td></td>
-                          <td>{item.description}</td>
+                          <td className="table-description">{item.description}</td>
                           <td>{item.units}</td>
                           <td>unidades</td>
-                          <td>{Number(item.unitValue).toFixed(2)}</td>
+                          <td>{Number(item.unitValue! * (invoice.invoiceType === 'A' ? 100/(100 + +vatValues[item.iva!]) : 1)).toFixed(2)}</td>
                           <td>0,00</td>
                           <td>0,00</td>
-                          <td>{+item.units! * +item.unitValue!}</td>
+                          <td>{(+item.units! * +(item.unitValue! * (invoice.invoiceType === 'A' ? 100/(100 + +vatValues[item.iva!]) : 1))).toFixed(2)}</td>
+                          {invoice.invoiceType === 'A' && (
+                            <>
+                              <td>{isNaN(item.iva! as number) ? item.iva : `${item.iva}%`}</td>
+                              <td>{+item.units! * +item.unitValue!}</td>
+                            </>
+                          )}
                         </tr>
                       ) 
                     })}
@@ -137,7 +171,9 @@ function PDFDownloadV2({invoice, cuit}: {invoice: Invoice, cuit: CuitAccount}) {
               <div className="borderDiv invoiceTotal vat-description">
                 <div className="vat-types">
                   {['A', 'NOTA_CREDITO_A'].includes(invoice.invoiceType) ? (<>
-                      <div className="label">Subtotal: </div><div>$ {total.grossAmount}</div>
+                      <div className="label">Importe Excento: </div><div>$ {total.excentAmount}</div>
+                      <div className="label">Importe No gravado: </div><div>$ {total.notTaxedAmount}</div>
+                      <div className="label">Importe Neto gravado: </div><div>$ {total.grossAmount}</div>
                       <div className="label">Importe otros tributos: </div> <div>$ 0</div>
                       {Object.entries(total.iva).map(([ivaAmount, ivaValue]) => (
                         <>
@@ -151,7 +187,8 @@ function PDFDownloadV2({invoice, cuit}: {invoice: Invoice, cuit: CuitAccount}) {
                   <div className="label">Importe total: </div> <div>$ {total.netAmount}</div>
                 </div>
               </div>
-              <div className="invoiceTotal">
+              <div className="invoice-footer">
+                <div className="label">Fecha Vto. de CAE: {invoice.caeExpirationDate ? format(parseISO(invoice.caeExpirationDate), 'dd/MM/yyyy') : ''}</div>
                 <div className="label">CAE N°: {invoice.cae}</div>
               </div>
             </div>
